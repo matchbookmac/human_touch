8ad9eb83c62a9a419fe39f5b0e93bf5916caa909
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-07-15 16:39:48 -0700

    Ultra-minimalist styling

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -6,5 +6,5 @@ source.addEventListener('message', function(e) {
   var message;
   message = $.parseJSON(e.data).body;
   user = $.parseJSON(e.data).username;
-  $('#chat').append($("<li>" + user + ": " + message + '</li>'));
+  $('#chat').prepend($("<li>" + user + ": " + message + '</li>'));
 });
